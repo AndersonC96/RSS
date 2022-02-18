c00a614439024a50ff81a504a751e9c8cc905b18
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-02-18 16:19:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php
     require __DIR__ . '/vendor/autoload.php';// Carregar automaticamente os arquivos com o Composer
+    require __DIR__ . '/includes/header.php';// Carregar o header
+    require __DIR__ . '/includes/feed.php';// Carregar o feed
+    require __DIR__ . '/includes/footer.php';// Carregar o footer
